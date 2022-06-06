3d27c83dc23bae25eeadb2a627aa15f9476984eb
MANSU.J <seunghyun.jung.84@gmail.com>
2022-06-06 23:04:49 +0900

    Refactor: 루틴 타입
    - 루틴 인스턴스 메인파일로 이동

--- CodeStarterCamp_Week4/Routine.swift
+++ CodeStarterCamp_Week4/Routine.swift
@@ -3,16 +3,4 @@ import Foundation
 struct Routine {
 	let routineName: String
 	let exercises: [Exercise]
-	
-	mutating func playMyRoutine(condition: BodyCondition) {
-		print("""
-		--------------
-		hellRoutine을 시작합니다.
-		""")
-		for exercise in exercises {
-			print("\(exercise.name)")
-			exercise.action()
-		}
-		condition.printCondition()
-	}
 }

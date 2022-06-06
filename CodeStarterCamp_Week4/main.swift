342110ee51a7ad6197e95bf8416cf8855082ee53
MANSU.J <seunghyun.jung.84@gmail.com>
2022-06-06 23:02:49 +0900

    Refactor: 메인파일
    - 메인파일에 루틴 이동

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -1,6 +1,13 @@
 import Foundation
 
-var myBodyCondition = BodyCondition()
-var myRoutine = Routine(routineName: "hellRoutine", exercises: [윗몸일으키기, 윗몸일으키기, 동적휴식, 스쿼트, 스쿼트, 동적휴식, 오래달리기, 오래달리기])
+var hellRoutine = Routine(routineName: "지옥의 루틴",
+						  exercises: [윗몸일으키기, 윗몸일으키기, 동적휴식, 스쿼트, 스쿼트, 동적휴식, 오래달리기, 오래달리기])
+var ohMyGodRoutine = Routine(routineName: "오마이갓 루틴",
+							 exercises: [윗몸일으키기, 윗몸일으키기, 동적휴식, 스쿼트, 스쿼트, 동적휴식])
+var routine: [Routine] = [hellRoutine, ohMyGodRoutine]
 
-myRoutine.playMyRoutine(condition: myBodyCondition)
+var gym = FitnessCenter(centerName: "야곰 피트니스 센터")
+gym.enterTheGym()
+
+
+

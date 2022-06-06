a25a97408a213608037d77fffa9f07ab55a719e4
MANSU.J <seunghyun.jung.84@gmail.com>
2022-06-06 23:05:59 +0900

    Refactor: 바디컨디션 타입
    - 기본 프로파티만 기재

--- CodeStarterCamp_Week4/BodyCondition.swift
+++ CodeStarterCamp_Week4/BodyCondition.swift
@@ -1,19 +1,15 @@
 import Foundation
 
 class BodyCondition {
-	var upperMuscle: Int = 0
-	var lowerMuscle: Int = 0
-	var endurance: Int = 0
-	var fatigue: Int = 0
+	var upperMuscle: Int
+	var lowerMuscle: Int
+	var endurance: Int
+	var fatigue: Int
 	
-	func printCondition() {
-	print("""
-		--------------
-		현재의 컨디션은 다음과 같습니다.
-		상체근력: \(upperMuscle)
-		하체근력: \(lowerMuscle)
-		근지구력: \(endurance)
-		피로도: \(fatigue)
-		""")
+	init() {
+		upperMuscle = 0
+		lowerMuscle = 0
+		endurance = 0
+		fatigue = 0
 	}
 }

fdf4dea2f27b98c8c1a7a40e9522837ad60c9224
MANSU.J <seunghyun.jung.84@gmail.com>
2022-06-06 23:10:26 +0900

    Refactor: 운동 타입
    - 액션 클로저를 포함한 운동 인스턴스 생성

--- CodeStarterCamp_Week4/Exercise.swift
+++ CodeStarterCamp_Week4/Exercise.swift
@@ -2,26 +2,26 @@ import Foundation
 
 struct Exercise {
 	let name: String
-	let action: () -> Void
+	let action: (BodyCondition) -> Void
 }
 
-let 윗몸일으키기: Exercise = Exercise(name: "윗몸일으키기") {
-	myBodyCondition.upperMuscle += Int.random(in: 10...20)
-	myBodyCondition.fatigue += Int.random(in: 10...20)
+let 윗몸일으키기: Exercise = Exercise(name: "윗몸일으키기") { person in
+	person.upperMuscle += Int.random(in: 10...20)
+	person.fatigue += Int.random(in: 10...20)
 }
 
-let 스쿼트: Exercise = Exercise(name: "스쿼트") {
-	myBodyCondition.lowerMuscle += Int.random(in: 20...30)
-	myBodyCondition.fatigue += Int.random(in: 10...20)
+let 스쿼트: Exercise = Exercise(name: "스쿼트") { person in
+	person.lowerMuscle += Int.random(in: 20...30)
+	person.fatigue += Int.random(in: 10...20)
 }
 
-let 오래달리기: Exercise = Exercise(name: "오래달리기") {
-	myBodyCondition.upperMuscle += Int.random(in: 5...10)
-	myBodyCondition.lowerMuscle += Int.random(in: 5...10)
-	myBodyCondition.endurance += Int.random(in: 20...30)
-	myBodyCondition.fatigue += Int.random(in: 20...30)
+let 오래달리기: Exercise = Exercise(name: "오래달리기") { person in
+	person.upperMuscle += Int.random(in: 5...10)
+	person.lowerMuscle += Int.random(in: 5...10)
+	person.endurance += Int.random(in: 20...30)
+	person.fatigue += Int.random(in: 20...30)
 }
 
-let 동적휴식: Exercise = Exercise(name: "동적휴식") {
-	myBodyCondition.fatigue -= Int.random(in: 10...20)
+let 동적휴식: Exercise = Exercise(name: "동적휴식") { person in
+	person.fatigue -= Int.random(in: 10...20)
 }
